33340e77823f122722cff8dfd632fb191b265c5f
fabiano lima <fabianosl.dev@gmail.com>
2024-05-22 01:41:24 -0300

    style: format

--- backend/src/app/assignor/assignor.module.ts
+++ backend/src/app/assignor/assignor.module.ts
@@ -1,15 +1,11 @@
-import { Module } from "@nestjs/common";
-import { AssignorService } from "./assignor.service";
-import { AssignorRepository } from "./assignor.repository";
-import { AssignorController } from "./assignor.controller";
-import { PrismaService } from "src/config/prisma/prisma.service";
+import { Module } from '@nestjs/common';
+import { AssignorService } from './assignor.service';
+import { AssignorRepository } from './assignor.repository';
+import { AssignorController } from './assignor.controller';
+import { PrismaService } from 'src/config/prisma/prisma.service';
 
 @Module({
     controllers: [AssignorController],
-    providers: [
-        AssignorService, 
-        AssignorRepository, 
-        PrismaService
-    ]
+    providers: [AssignorService, AssignorRepository, PrismaService],
 })
 export class AssignorModule {}
